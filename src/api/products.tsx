4237b79ab9b1df7005a9b67cdc440a61d5060bd8
AguirreGaston24 <aguirregastona@hotmail.com>
2024-07-31 02:32:20 -0300

    first commit

--- src/api/products.tsx
+++ src/api/products.tsx
@@ -1,7 +1,7 @@
 import { CreateProducts, UpdateProducts } from "../interface/products.interface";
 
 // Configuración de la API
-const API = "https://jumahumitosback.onrender.com/";
+const API = "https://jumahumitosback.onrender.com";
 
 // Función para obtener productos
 export const getProductsRequest = async () => {
